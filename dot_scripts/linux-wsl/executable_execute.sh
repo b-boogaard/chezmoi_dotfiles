359c9973bd6becb9c2966a0dec3001a2ad8af84a
Brian van de Boogaard <brian.boogaard@gmail.com>
2021-12-03 15:14:09 -0700

    Rename for ordering, run install-java.sh

--- dot_scripts/linux-wsl/executable_execute.sh
+++ dot_scripts/linux-wsl/executable_execute.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+
 sh $(pwd)/linux-wsl/install-packages.sh \
  && sh $(pwd)/linux-wsl/setup-shell.sh \
- && sh $(pwd)/linux-wsl/setup-vim.sh
+ && sh $(pwd)/linux-wsl/setup-vim.sh \
+ && sh $(pwd)/linux-wsl/install-java.sh
