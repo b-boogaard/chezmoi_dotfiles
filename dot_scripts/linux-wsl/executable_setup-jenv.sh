e9f62e7ba94d1b2885892df044ee91fa3d9bd1e7
Brian van de Boogaard <brian.boogaard@gmail.com>
2021-12-03 23:27:17 -0700

    Update how jenv is called

--- dot_scripts/linux-wsl/executable_setup-jenv.sh
+++ dot_scripts/linux-wsl/executable_setup-jenv.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env zsh
 
 echo "Adding zulu11 to jenv."
-jenv_java_version=$(jenv version)
+jenv_java_version=$(zsh -c "jenv version")
 
 if [[ $jenv_java_version =~ "11" ]]; then
   echo "jenv already set."
   return
 else
-  $HOME/.jenv/bin/jenv add "/usr/lib/jvm/zulu11"
+  zsh -c "$HOME/.jenv/bin/jenv add '/usr/lib/jvm/zulu11'"
 
   echo "Setting global as 11 in jenv."
-
-  jenv global 11
+  zsh -c "jenv global 11"
 fi
