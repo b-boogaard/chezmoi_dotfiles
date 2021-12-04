da086e7487cba40cf7fbedafc9cc993e9040ac0e
Brian van de Boogaard <brian.boogaard@gmail.com>
2021-12-03 23:46:26 -0700

    Remove install plugins for vim

--- dot_scripts/linux-wsl/executable_setup-vim.sh
+++ dot_scripts/linux-wsl/executable_setup-vim.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-echo "Installing vim plugins"
-sleep 1s
-
-if [ -d "$HOME/.vim/bundle" ]; then
-  echo "Vim plugins already installed."
-else
-  vim -c "PlugInstall" -c "quit" -c "quit"
-fi
+# echo "Installing vim plugins"
+# sleep 1s
+#
+# if [ -d "$HOME/.vim/bundle" ]; then
+#   echo "Vim plugins already installed."
+# else
+#   vim -c "PlugInstall" -c "quit" -c "quit"
+# fi
--- dot_scripts/run_after_50_switch-to-zsh.sh
+++ dot_scripts/run_after_50_switch-to-zsh.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
-echo "Don't forget to switch your current shell by either restarting it or running zsh."
+# echo "Don't forget to switch your current shell by either restarting it or running zsh."
+zsh -i -c "exit"
