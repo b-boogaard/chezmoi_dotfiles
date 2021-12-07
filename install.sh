af4cb550efa9ffc38c723800dff2ebc6ccdc9513
Brian van de Boogaard <brian.boogaard@gmail.com>
2021-12-06 20:46:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-sudo apt install -y git \
+sudo apt -qq install -y git \
   && sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply https://github.com/b-boogaard/chezmoi_dotfiles.git
